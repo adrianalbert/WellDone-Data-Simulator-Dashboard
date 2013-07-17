8c1b30b8f63c1bc369b3a0e3bad946e48532ee8d
Adrian Albert <adrian.t.albert@gmail.com>
2013-07-16 20:18:26 -0700

    nitial commit

---
+++ ui.r
@@ -0,0 +1,59 @@
+
+###########################################
+# ui.r
+# 
+# MoMo data mock-up visualizations in R.
+# This is the user interface file for RShiny.
+# 
+# Adrian Albert
+# Last modified: June 2013.
+###########################################
+
+library(shiny)
+library(shinyIncubator)
+options(RCHART_LIB = 'polycharts')
+
+# this should come from the server side
+sensor_list = list("Gereb Arso Village" = 1, "Eytay Chinu" = 2,
+                   "Mifray Derahu Village" = 3, "Letslatsso Village" = 4)
+
+# Define UI for random distribution application 
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("MoMo Dashboard"),
+  
+  # Sidebar with controls to select the random distribution type
+  # and number of observations to generate. Note the use of the br()
+  # element to introduce extra vertical spacing
+  
+  sidebarPanel(
+    
+    # select sensor location
+    selectInput("ID", "Sensor Location:", sensor_list),    
+    br(), 
+    
+    # prepare Slidify/Sweave LaTex pdf report
+    downloadButton('prepareReportBtn', 'Prepare Report'),
+    br(),
+    
+    # plot map
+    div(class="span16", style="margin-top:50px", 
+        plotOutput("map.alerts")),
+    br()    
+  ),
+  
+  # Show a tabset that includes a plot, summary, and table view
+  # of the generated distribution
+  
+  mainPanel(
+    tabsetPanel(
+      tabPanel("General View", plotOutput("general")), 
+      tabPanel("Sensor Locations", plotOutput("map.tot")), 
+      tabPanel("Weather Data", plotOutput("weather.series")), 
+      tabPanel("Sensor View", showOutput("sensor", "polycharts")), 
+      tabPanel("Summary Statistics", tableOutput("summary")),
+      tabPanel("Raw data", tableOutput("raw"))
+    )
+  )
+))
